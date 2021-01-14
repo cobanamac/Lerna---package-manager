5a9c6f15e86bec48f54fdb4364109c876a7f7aba
René Leonard Aasen <reneleonardaasen@gmail.com>
2021-01-14 15:18:09 +0100

    tiny - added index

--- packages/tiny/index.js
+++ packages/tiny/index.js
@@ -1,4 +1,4 @@
-module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  return string.replace(/\s/g, "");
+module.exports = () => {
+  console.log("hello from tiny");
+  console.log("hello from tiny");
 };
